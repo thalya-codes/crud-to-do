d33696ae28512e66f2f4a31b3252a509fe296fe2
Thalya Stéffany <thalyacoiimbra@gmail.com>
2022-04-21 10:49:11 -0300

    Implementação da feature de editar tarefa + ajustes no estilo

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ window.onload = () => {
     }
 }
 
-
 function adicionarTarefa() {
     const input  = $('[data-input]')
     const naoExiste = localStorage.getItem('tarefas') == null
@@ -21,7 +20,7 @@ function adicionarTarefa() {
     } else {
         localStorage.setItem('tarefas', JSON.stringify(tarefas))
     }
-
+    input.value = ''
     exibirTarefa()
 }
 
@@ -29,14 +28,43 @@ function exibirTarefa() {
     lista.innerHTML = ''
 
     tarefas.map((tarefa, index) => {
-        lista.innerHTML += `<li id='${index}'}>
-        <input type='checkbox' onclick='concluirTarefa(${index})'/>
-        ${tarefa}
-        <button onclick='deletarTarefa(${index})'>Deletar</button>
+        lista.innerHTML += `<li id='${index}' class='tarefas li'>
+            <div class='container-tarefas'>
+                <input type='checkbox' onclick='concluirTarefa(${index})'/>
+                <span class='tarefas span'>${tarefa}</span>
+                <div class='container-btn'>
+                    <button class='btn-editar' onclick='editarTarefa(${index})'>Editar</button>
+                    <button onclick='deletarTarefa(${index})' class='btn-deletarTarefa'>Deletar</button>  
+                </div>
+            </div>
         </li>`
-
     })
    
+}
+
+
+
+function editarTarefa(id) {
+    const input = document.createElement('input')
+    input.type='text'
+    input.classList.add('input')
+    input.classList.add
+    let spanTarefas = document.querySelectorAll('.span')
+    const li = document.querySelectorAll('.li')
+
+
+    input.value = spanTarefas[id].innerText
+
+    input.addEventListener('change', (e) => {
+         spanTarefas[id].innerText = e.target.value
+  
+        if(localStorage.getItem('tarefas') != null) {
+            localStorage.setItem('tarefas', JSON.stringify(tarefas))
+            exibirTarefa()
+        }
+    })
+
+    li[id].appendChild(input)
 }
 
 function deletarTarefa(id) {
@@ -50,14 +78,14 @@ function deletarTarefa(id) {
 }
 
 function concluirTarefa(id) {
-    if(tarefas[id].includes('<strike>')) {
-        tarefas[id] = tarefas[id].replace('<strike>', '')
+    if(tarefas[id].includes(`<strike class='tarefas'>`)) {
+        tarefas[id] = tarefas[id].replace(`<strike class='tarefas'>`, '')
         tarefas[id] = tarefas[id].replace('</strike>', '')
         if(localStorage.getItem('tarefas') != null) {
             localStorage.setItem('tarefas', JSON.stringify(tarefas))
         }
     } else {
-        tarefas[id] = `<strike>${tarefas[id]}</strike>`
+        tarefas[id] = `<strike class='tarefas'>${tarefas[id]}</strike>`
         if(localStorage.getItem('tarefas') != null) {
             localStorage.setItem('tarefas', JSON.stringify(tarefas))
         }
@@ -69,13 +97,3 @@ function concluirTarefa(id) {
 // e preciso atualizar os dados do storage
 //solução alternativa concluir tarefa => adicionar um checkbox javascript
 
-
-
-/*
-
-        lista.innerHTML += `<li id='${index}'}>
-        <input type='checkbox' onclick='concluirTarefa(${index})'/>
-        ${tarefa}
-        <button onclick='deletarTarefa(${index})'>Deletar</button>
-        </li>`
-*/
